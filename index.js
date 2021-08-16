0055e141bc0fd761dc925240a2ad20b2fcbfe2b3
jpgsaraceni <jpgomes@id.uff.br>
2021-08-16 11:16:53 -0300

    Object oriented

--- index.js
+++ index.js
@@ -1,282 +1,299 @@
-/* eslint-disable no-console */
+/* eslint-disable import/extensions */
+
+// TODO:
+// add optional fields to inserts/updates;
+// transactions;
+// validations;
+// duplicate db inserts;
+// review query error handling;
+// id is incrementing even when query fails;
+
 /* -------------------------------------------------------------------------- */
 /*                               ANCHOR imports                               */
 /* -------------------------------------------------------------------------- */
 
-import dotenv from 'dotenv';
-import pkg from 'pg';
 import express from 'express';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
+import PostgreCrud from './crud.js';
 
-dotenv.config();
-const secret = process.env.JWT_SECRET;
-const poolConfig = JSON.parse(process.env.POOL_CONFIG);
-const { Pool } = pkg;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 
 /* -------------------------------------------------------------------------- */
-/*                ANCHOR instance and configure an object of Pool             */
-/* -------------------------------------------------------------------------- */
-
-const pool = new Pool(poolConfig);
-
-pool.on('error', (err, client) => {
-  console.error(`Client: ${client} \n Error: ${err}`);
-  process.exit(-1);
-});
-
-/* -------------------------------------------------------------------------- */
 /*                                ANCHOR login                                */
 /* -------------------------------------------------------------------------- */
 
+const postgresLogin = new PostgreCrud();
+
 app.get('/login', (req, res) => {
   const { email } = req.body;
-  const { password: frontPassword } = req.body;
+  const { password } = req.body;
 
   const query = 'SELECT id, name, password'
     + ' FROM users'
     + ' WHERE $1 = email AND deleted = false';
   const values = [email];
 
-  pool.connect()
-    .then((client) => client.query(query, values)
-      .then((result) => {
-        const { name, password: dbPassword, id } = result.rows[0];
-
-        bcrypt.compare(frontPassword, dbPassword).then((bcryptResult) => {
-          if (bcryptResult) {
-            jwt.sign({ id }, secret, (err, token) => {
-              if (err) {
-                console.log(`JWT failed: ${err}`);
-                res.sendStatus(500);
-              } else {
-                res.cookie('token', token);
-                res.status(200).send(`Hello, ${name}!`);
-              }
-            });
-          } else {
-            res.sendStatus(401);
-          }
-        }).catch((err) => {
-          console.log(`Bcrypt failed: ${err}`);
-          res.sendStatus(500);
-        });
-        client.release();
-      })
-
-      .catch((err) => {
-        client.release();
-        console.log(`Query failed: ${err}`);
-        res.sendStatus(500);
-      }))
-
-    .catch((err) => {
-      console.log(`Connection failed: ${err.message}`);
-      res.sendStatus(500);
-    });
+  postgresLogin.setParams(req, res);
+  postgresLogin.login(password, query, values);
 });
-
-/* -------------------------------------------------------------------------- */
-/*                        ANCHOR Pool connect function                        */
-/* -------------------------------------------------------------------------- */
-
-function connectCRUD(query, values, res) {
-  pool.connect()
-    .then((client) => client.query(query, values)
-      .then((result) => res.status(200).send(result.rows))
-      .catch((queryError) => {
-        console.log(queryError);
-        res.sendStatus(500);
-      }))
-    .catch((connectionError) => {
-      console.log(connectionError);
-      res.sendStatus(500);
-    });
-}
 
 /* -------------------------------------------------------------------------- */
 /*                              ANCHOR CRUD users                             */
 /* -------------------------------------------------------------------------- */
 
+const crudUsers = new PostgreCrud();
+
 app.post('/users', (req, res) => {
-  const { token } = req.cookies;
-  const { password: receivedPassword } = req.body;
-  const values = [req.body.name, req.body.email];
+  const receivedPassword = req.body.password;
+  const values = [
+    req.body.name,
+    req.body.email,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  bcrypt.hash(receivedPassword, 10, (bcryptError, hash) => {
+    if (bcryptError) throw bcryptError;
+    values.push(hash);
 
-      bcrypt.hash(receivedPassword, 10, (bcryptError, hash) => {
-        if (bcryptError) throw bcryptError;
-        values.push(hash);
+    const query = 'INSERT INTO users'
+      + ' (name, email, password, create_user_id)'
+      + ' VALUES'
+      + ' ($1, $2, $3, $4)'
+      + ' RETURNING name, email, password, create_user_id';
 
-        const query = 'INSERT INTO users'
-          + ' (name, email, password, create_user_id)'
-          + ' VALUES'
-          + ' ($1, $2, $3, $4)'
-          + ' RETURNING name, email, password, create_user_id';
-        values.push(userId);
-
-        connectCRUD(query, values, res);
-      });
-    }
+    crudUsers.setParams(req, res);
+    crudUsers.request(query, values);
   });
 });
 
 app.get('/users', (req, res) => {
-  const { token } = req.cookies;
+  const values = [];
 
-  jwt.verify(token, secret, (err) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const query = 'SELECT * FROM users WHERE deleted = false';
-      const values = [];
+  const query = 'SELECT * FROM users WHERE deleted = false';
 
-      connectCRUD(query, values, res);
-    }
-  });
+  crudUsers.setParams(req, res);
+  crudUsers.setNoId(true);
+  crudUsers.request(query, values);
 });
 
 app.put('/users', (req, res) => {
-  const { token } = req.cookies;
-  const { password: receivedPassword } = req.body;
-  const values = [req.body.name, req.body.email, req.body.id];
+  const receivedPassword = req.body.password;
+  const values = [
+    req.body.name,
+    req.body.email,
+    req.body.id,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  bcrypt.hash(receivedPassword, 10, (bcryptError, hash) => {
+    if (bcryptError) throw bcryptError;
+    values.push(hash);
 
-      bcrypt.hash(receivedPassword, 10, (bcryptError, hash) => {
-        if (bcryptError) throw bcryptError;
-        values.push(hash);
+    const query = 'UPDATE users'
+      + ' SET name=$1, email=$2, password=$4, update_date=NOW(), update_user_id=$5'
+      + ' WHERE id=$3 AND deleted=false'
+      + ' RETURNING name, email, password, create_user_id, create_date, update_user_id, update_date';
 
-        const query = 'UPDATE users'
-          + ' SET name=$1, email=$2, password=$4, update_date=NOW(), update_user_id=$5'
-          + ' WHERE id=$3 AND deleted=false'
-          + ' RETURNING name, email, password, create_user_id, create_date, update_user_id, update_date';
-        values.push(userId);
-
-        connectCRUD(query, values, res);
-      });
-    }
+    crudUsers.setParams(req, res);
+    crudUsers.request(query, values);
   });
 });
 
 app.delete('/users', (req, res) => {
-  const { token } = req.cookies;
-  const values = [req.body.id];
+  const values = [
+    req.body.id,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  const query = 'UPDATE users'
+    + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
+    + ' WHERE id=$1'
+    + ' RETURNING id, name, deleted';
 
-      const query = 'UPDATE users'
-        + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
-        + ' WHERE id=$1'
-        + ' RETURNING id, name, deleted';
-      values.push(userId);
-
-      connectCRUD(query, values, res);
-    }
-  });
+  crudUsers.setParams(req, res);
+  crudUsers.request(query, values);
 });
 
 /* -------------------------------------------------------------------------- */
 /*                            ANCHOR CRUD suppliers                           */
 /* -------------------------------------------------------------------------- */
-// TODO: create a middleware for jwt token verification.
-// add optional fields (email and phone_number)
+
+const crudSuppliers = new PostgreCrud();
 
 app.post('/suppliers', (req, res) => {
-  const { token } = req.cookies;
-  const values = [req.body.name, req.body.city, req.body.state];
+  const values = [
+    req.body.name,
+    req.body.city,
+    req.body.state,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  const query = 'INSERT INTO suppliers'
+    + ' (name, city, state, create_user_id)'
+    + ' VALUES'
+    + ' ($1, $2, $3, $4)'
+    + ' RETURNING name, city, state, create_user_id';
 
-      const query = 'INSERT INTO suppliers'
-        + ' (name, city, state, create_user_id)'
-        + ' VALUES'
-        + ' ($1, $2, $3, $4)'
-        + ' RETURNING name, city, state, create_user_id';
-      values.push(userId);
-
-      connectCRUD(query, values, res);
-    }
-  });
+  crudSuppliers.setParams(req, res);
+  crudSuppliers.request(query, values);
 });
 
 app.get('/suppliers', (req, res) => {
-  const { token } = req.cookies;
+  const values = [];
 
-  jwt.verify(token, secret, (err) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const query = 'SELECT * FROM suppliers WHERE deleted = false';
-      const values = [];
+  const query = 'SELECT * FROM suppliers WHERE deleted = false';
 
-      connectCRUD(query, values, res);
-    }
-  });
+  crudSuppliers.setParams(req, res);
+  crudSuppliers.setNoId(true);
+  crudSuppliers.request(query, values);
 });
 
 app.put('/suppliers', (req, res) => {
-  const { token } = req.cookies;
-  const values = [req.body.supplierId, req.body.name];
+  const values = [
+    req.body.supplierId,
+    req.body.name,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  const query = 'UPDATE suppliers'
+    + ' SET name=$2, update_date=NOW(), update_user_id=$3'
+    + ' WHERE id=$1 AND deleted=false'
+    + ' RETURNING id, name, city, state';
 
-      const query = 'UPDATE suppliers'
-        + ' SET name=$2, update_date=NOW(), update_user_id=$3'
-        + ' WHERE id=$1 AND deleted=false'
-        + ' RETURNING id, name, city, state';
-      values.push(userId);
-
-      connectCRUD(query, values, res);
-    }
-  });
+  crudSuppliers.setParams(req, res);
+  crudSuppliers.request(query, values);
 });
 
 app.delete('/suppliers', (req, res) => {
-  const { token } = req.cookies;
-  const values = [req.body.supplierId];
+  const values = [
+    req.body.supplierId,
+  ];
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      res.sendStatus(401);
-    } else {
-      const { id: userId } = decoded;
+  const query = 'UPDATE suppliers'
+    + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
+    + ' WHERE id=$1'
+    + ' RETURNING id, name, deleted';
 
-      const query = 'UPDATE suppliers'
-        + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
-        + ' WHERE id=$1'
-        + ' RETURNING id, name, deleted';
-      values.push(userId);
+  crudSuppliers.setParams(req, res);
+  crudSuppliers.request(query, values);
+});
 
-      connectCRUD(query, values, res);
-    }
-  });
+/* -------------------------------------------------------------------------- */
+/*                         ANCHOR CRUD product groups                         */
+/* -------------------------------------------------------------------------- */
+
+const crudProductGroups = new PostgreCrud();
+
+app.post('/product-groups', (req, res) => {
+  const values = [req.body.name];
+
+  const query = 'INSERT INTO product_groups'
+    + ' (name, create_user_id)'
+    + ' VALUES'
+    + ' ($1, $2)'
+    + ' RETURNING id, name, create_user_id, create_date';
+
+  crudProductGroups.setParams(req, res);
+  crudProductGroups.request(query, values);
+});
+
+app.get('/product-groups', (req, res) => {
+  const values = [];
+
+  const query = 'SELECT * FROM product_groups WHERE deleted = false';
+
+  crudProductGroups.setParams(req, res);
+  crudProductGroups.setNoId(true);
+  crudProductGroups.request(query, values);
+});
+
+app.put('/product-groups', (req, res) => {
+  const values = [
+    req.body.productGroupId,
+    req.body.name,
+  ];
+
+  const query = 'UPDATE product_groups'
+    + ' SET name=$2, update_date=NOW(), update_user_id=$3'
+    + ' WHERE id=$1 AND deleted=false'
+    + ' RETURNING id, name, create_user_id, create_date, update_user_id, update_date';
+
+  crudProductGroups.setParams(req, res);
+  crudProductGroups.request(query, values);
+});
+
+app.delete('/product-groups', (req, res) => {
+  const values = [
+    req.body.productGroupId,
+  ];
+
+  const query = 'UPDATE product_groups'
+    + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
+    + ' WHERE id=$1'
+    + ' RETURNING id, name, deleted';
+
+  crudProductGroups.setParams(req, res);
+  crudProductGroups.request(query, values);
+});
+
+/* -------------------------------------------------------------------------- */
+/*                            ANCHOR CRUD products                            */
+/* -------------------------------------------------------------------------- */
+
+const crudProducts = new PostgreCrud();
+
+app.post('/products', (req, res) => {
+  const values = [
+    req.body.id,
+    req.body.name,
+  ];
+
+  const query = 'INSERT INTO products' // check if product group is not deleted
+    + ' (product_group_id, name, create_user_id)'
+    + ' VALUES'
+    + ' ($1, $2, $3)'
+    + ' RETURNING id, product_group_id, name, create_user_id, create_date';
+
+  crudProducts.setParams(req, res);
+  crudProducts.request(query, values);
+});
+
+app.get('/products', (req, res) => {
+  const values = [];
+
+  const query = 'SELECT * FROM products WHERE deleted = false';
+
+  crudProducts.setParams(req, res);
+  crudProducts.setNoId(true);
+  crudProducts.request(query, values);
+});
+
+app.put('/products', (req, res) => {
+  const values = [
+    req.body.productId,
+    req.body.name,
+  ];
+
+  const query = 'UPDATE products'
+    + ' SET name=$2, update_date=NOW(), update_user_id=$3'
+    + ' WHERE id=$1 AND deleted=false'
+    + ' RETURNING id, name, create_user_id, create_date, update_user_id, update_date';
+
+  crudProducts.setParams(req, res);
+  crudProducts.request(query, values);
+});
+
+app.delete('/products', (req, res) => {
+  const values = [
+    req.body.productId,
+  ];
+
+  const query = 'UPDATE products'
+    + ' SET deleted=true, update_date=NOW(), update_user_id=$2'
+    + ' WHERE id=$1'
+    + ' RETURNING id, name, deleted';
+
+  crudProducts.setParams(req, res);
+  crudProducts.request(query, values);
 });
 
 app.listen(80);
